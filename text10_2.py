a6516ab39879eb3d3bc0fc813b6c11292efd2324
lizeqing <lizeqing77@163.com>
2021-10-03 11:07:32 +0800

    updata my text

--- text10_2.py
+++ text10_2.py
@@ -5,12 +5,10 @@ import os
 
 '''
 思路：
+    获取网页地址
     下载页面文本
-    获取图片地址
     爬取图片并保存
 '''
-
-
 # 获取网址
 def getUrl(url):
     try:
@@ -21,24 +19,36 @@ def getUrl(url):
     except:
         return "连接失败"
 
+# 下载网页页面
+def getHtml(html):
+    bs = BeautifulSoup(html ,"html.parser")
+    path = 'text_Flask/templates/download_html.html' #保存页面的相对路径
+    if not os.path.exists(path):
+        fb = open(path, 'w+', encoding='utf-8')
+        fb.write(bs.prettify())
+        print("页面下载成功")
+    else:
+        print("文件已经存在")
+
 
 # 获取图片地址并保存下载
-def getPic(html):
+def getPic(html,url):
     soup = BeautifulSoup(html, "html.parser")
     # 通过分析页面内容 查找img标签
     all_img = soup.find_all('img')  # 查找所以的img标签
     for img in all_img:
-        src = img['src']  # 获取img标签里的src内容
-        img_url = src
+        src = img['src'][2:]  # 获取img标签里的src内容
+        img_url = url + src
         print(img_url)
         root = "D:/Mylearn/InternetWorm/text_Flask/static/images/"  # 保存图片的路径
         path = root + img_url.split('/')[-1]  # 获取img的文件名
         print(path)
         try:
-            # if not os.path.exists(root):
-            #     os.mkdir(root)
+            if not os.path.exists(root):
+                os.mkdir(root)
             if not os.path.exists(path):
                 read = requests.get(img_url)
+                # print(read)
                 with open(path, 'wb') as f:
                     f.write(read.content)
                     f.close()
@@ -48,8 +58,10 @@ def getPic(html):
         except:
             print("文件爬取失败！")
 
-
 # 主函数
 if __name__ == '__main__':
-    html_url = getUrl("http://pythonscraping.com/pages/page3.html")
-    getPic(html_url)
+    url1 = "http://pythonscraping.com/pages/page3.html"
+    url2 = "http://pythonscraping.com"
+    html_url = getUrl(url1) #获取网址
+    getHtml(html_url)   #下载网页页面
+    getPic(html_url,url2)   #下载网页图片
