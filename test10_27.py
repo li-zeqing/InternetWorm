716568af506903b7241c7550e86213b0ece64ed4
lizeqing <lizeqing77@163.com>
2021-10-31 23:07:07 +0800

    update

--- test10_27.py
+++ test10_27.py
@@ -1,13 +1,32 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import bs4
 
 
 def getHtml(url):
     resp = urlopen(url)
-    return BeautifulSoup(resp.read(),'html.parser')
+    bs = BeautifulSoup(resp.read(),'html.parser')
+    print(type(bs))
+    print(type(bs).__name__ )
+
+def getParse(url):
+    parse = urlparse(url)
+    print(parse)
+    print(type(parse))
+    print(parse.scheme)
+    print(parse.netloc)
+    print(parse.path)
+    print(type(parse.scheme))
 
 if __name__ == '__main__':
-    url = 'https://baike.baidu.com/'
-    bs = getHtml(url)
-    print(bs.prettify())
+    url1 = 'https://www.oreilly.com'
+    url2 = 'https://www.oreilly.com/online-learning/government.html'
+    url3 = 'https://www.oreilly.com/online-learning/support/content.html'
+    print(url3.split('/')[-1][-5:] == '.html')
+    print('/'.join(url3.split('/')[:-1]))
+    bs = getHtml(url1)
+    # getParse(url2)
+    getParse(url3)
+    # print(bs.prettify())
     # print(len(bs.find_all(attrs='href')))
