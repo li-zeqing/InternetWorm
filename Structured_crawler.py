a5a6bb5749b7d111d131ee4a9b4dd48dd78ec316
lizeqing <lizeqing77@163.com>
2021-11-10 22:27:50 +0800

    update

--- Structured_crawler.py
+++ Structured_crawler.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+"""
+通过搜索爬取网站数据
+通过主题搜索返回一个结果列表，再从该列表中逐个访问对应的链接，提取链接所对应页面的信息
+通过访问当当网 通过搜索python 获得一列表结果 逐一访问该列表获取 产品的作者和出版社
+"""
 
 class Content:
     """
@@ -14,8 +19,8 @@ class Content:
     def print(self):
         print("New article found for topic:{}".format(self.topic))
         print("URL:{}".format(self.url))
-        print("TITLE:{}".format(self.title))
-        print("BODY:\n{}".format(self.body))
+        print("作者:{}".format(self.title))
+        print("出版社:{}\n".format(self.body))
 
 
 
@@ -49,7 +54,7 @@ class Crawler:
     def safeGet(self, pageObj, selector):
         childObj = pageObj.select(selector)
         if childObj is not None and len(childObj) > 0:
-            return childObj[0].get_text()
+            return childObj[0].get_text()   # 每次返回所有匹配中的第一个
         return ""
 
     def search(self, topic, site):
@@ -62,25 +67,41 @@ class Crawler:
 
         bs = self.getPage(site.searchUrl + topic)
         searchResults = bs.select(site.resultListing)
+        i = 0
         for result in searchResults:
+            i = i + 1
+            print("{}\n".format(i))
+            # print(result)
             url = result.select(site.resultUrl)[0].attrs['href']
+            # print(url)
             # 检查一下是否为相对URL还是绝对URL
             if (site.absoluteUrl):
                 bs = self.getPage(url)
             else:
-                bs = self.getPage(site.url + url)
+                # bs = self.getPage(site.url + url)
+                bs = self.getPage('http:' + url)
+                # print("加入产品页面")
             if bs is None:
                 print("Something was wrong with that page or URL.Skipping!")
                 return
             title = self.safeGet(bs, site.titleTag)
             body = self.safeGet(bs, site.bodyTag)
             if title != '' and body != '':
-                content = Content(topic, title, body, url)
+                content = Content(topic, url, title, body)
                 content.print()
 if __name__ == '__main__':
+    # http://product.dangdang.com/699480210.html
+    # http://product.dangdang.com/1874319528.html
+
     crawler = Crawler()
 
-    siteData = ['dangdang', 'https://www.dangdang.com', 'http://search.dangdang.com/?key=', 'div#search_nature_rg > ul', 'li a', False, 'div.messbox_info span.t1 > a', 'div.messbox_info span[dd_name = "出版社"] > a']
+    siteData = ['dangdang', 'https://www.dangdang.com',
+                'http://search.dangdang.com/?key=',
+                'div#search_nature_rg > ul > li',
+                'p:nth-of-type(1) a',
+                False,
+                'div.messbox_info span[dd_name = "作者"] > a',
+                'div.messbox_info span[dd_name = "出版社"] > a']
 
     sites = []
 
@@ -95,3 +116,4 @@ if __name__ == '__main__':
         print("GETTING INFO ABOUT:" + topic)
         for targetSite in sites:
             crawler.search(topic, targetSite)
+
